8a40e754eb5f90e28db8fb971bfa2e27022ac51a
Dylan-Cabral-dev <cabraldylan16@gmail.com>
2026-04-07 19:02:49 -0300

    Add files via upload

---
+++ bucle_for_básico1.py.py
@@ -0,0 +1,47 @@
+# EJERCICIO 1
+print("EJERCICIO_1")
+
+for i in range (101):
+    print(i)
+
+# EJERCICIO 2
+print("EJERCICIO_2")
+
+for xd in range (2, 501, 2):
+    print (xd)
+
+# EJERCICIO 3
+print("EJERCICIO_3")
+
+for Dylan in range (1, 101):
+    if Dylan % 10 == 0:
+        print ("baby")
+    elif Dylan % 5 == 0:
+        print ("ice ice")
+    else:
+        print (Dylan) #
+
+# EJERCICIO 4
+print("EJERCICIO_4")
+
+a = 0
+for roberto in range (2, 500001, 2):
+        a += roberto
+print (a)
+
+# EJERCICIO 5
+print("EJERCICIO_5")
+
+for penelope in range (2024, 0, -3):
+    print (penelope)
+
+# EJERCICIO 6
+print("EJERCICIO_6")
+
+numinicial = 12
+numfinal = 351
+multiplo = 7
+
+for SEXTO in range (numinicial, numfinal + 1):
+    if SEXTO % multiplo == 0:
+        print (SEXTO)
